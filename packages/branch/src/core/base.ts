c64ab796967513005faca028b702d17c809fe34c
philomagi <tooppoo56@gmail.com>
2020-02-24 14:59:38 +0900

    refactor: unit implements of else-if

--- packages/branch/src/core/base.ts
+++ packages/branch/src/core/base.ts
@@ -4,62 +4,36 @@ import {
   SyncAction,
   AsyncBranch,
   EachCondition,
-  Action,
-  SyncElseifThen,
-  AsyncElseifThen
+  Action
 } from "../type";
-import {
-  ReadonlySyncTupleList,
-  ReadonlyAsyncTupleList,
-  AsyncTupleList
-} from "../lib/tuple";
+import { ReadonlySyncTupleList, AsyncTupleList } from "../lib/tuple";
 import { resolve } from "../lib/functions";
 
 export interface AsyncBranchConstructor<T> {
   new (tuples: AsyncTupleList<T>): AsyncBranch<T>;
 }
 
-interface SwitchMaybeElseIf {
-  <C, A, B>(update: Updater<C, A, B>): MaybeElseIf<C, A, B>;
-}
-type Updater<C, A, B> = (c: C, a: A) => B;
-interface MaybeElseIf<C, A, B> {
-  (c: C): { then(a: A): B };
-  (c: C, a: A): B;
-  (c: C, a?: A): B | { then(a: A): B };
-}
+type MaybeElseIf<Act, Branch> = Branch | { then(a: Act): Branch };
 
-const maybeElseIf: SwitchMaybeElseIf = <C, A, B>(update: Updater<C, A, B>) => (
-  c: C,
-  a?: A
-): any => {
-  if (!a) {
-    return {
-      then: (action: A): B => update(c, action)
-    };
-  }
-
-  return update(c, a);
-};
-
-export abstract class BaseSyncBranch<T> implements SyncBranch<T> {
+abstract class BaseBranch<Cond, Act, Branch, T> {
   protected constructor(protected readonly tuples: ReadonlySyncTupleList<T>) {}
 
-  private readonly maybeElseIf = maybeElseIf<
-    SyncEachCondition,
-    SyncAction<T>,
-    SyncBranch<T>
-  >((c, a) => this.update(c, a));
+  elseif(c: Cond): { then(a: Act): Branch };
+  elseif(c: Cond, a: Act): Branch;
+  elseif(c: Cond, a?: Act): MaybeElseIf<Act, Branch> {
+    if (!a) {
+      return { then: (action: Act): Branch => this.update(c, action) };
+    }
 
-  elseif(c: SyncEachCondition): SyncElseifThen<T>;
-  elseif(c: SyncEachCondition, a: SyncAction<T>): SyncBranch<T>;
-  elseif(
-    condition: SyncEachCondition,
-    action?: SyncAction<T>
-  ): SyncElseifThen<T> | SyncBranch<T> {
-    return this.maybeElseIf(condition, action);
+    return this.update(c, a);
   }
 
+  protected abstract update(c: Cond, action: Act): Branch;
+}
+
+export abstract class BaseSyncBranch<T>
+  extends BaseBranch<SyncEachCondition, SyncAction<T>, SyncBranch<T>, T>
+  implements SyncBranch<T> {
   else(otherAction: SyncAction<T>): T {
     const [, action] = this.tuples.find(([cond]) => resolve(cond)) || [];
 
@@ -77,21 +51,9 @@ export abstract class BaseSyncBranch<T> implements SyncBranch<T> {
   protected abstract readonly toAsync: AsyncBranchConstructor<T>;
 }
 
-export abstract class BaseAsyncBranch<T> implements AsyncBranch<T> {
-  constructor(protected readonly tuples: ReadonlyAsyncTupleList<T>) {}
-
-  private readonly maybeElseIf = maybeElseIf<
-    EachCondition,
-    Action<T>,
-    AsyncBranch<T>
-  >((c, a) => this.update(c, a));
-
-  elseif(c: EachCondition): AsyncElseifThen<T>;
-  elseif(c: EachCondition, a: Action<T>): AsyncBranch<T>;
-  elseif(c: EachCondition, a?: Action<T>): AsyncBranch<T> | AsyncElseifThen<T> {
-    return this.maybeElseIf(c, a);
-  }
-
+export abstract class BaseAsyncBranch<T>
+  extends BaseBranch<EachCondition, Action<T>, AsyncBranch<T>, T>
+  implements AsyncBranch<T> {
   async else(otherAction: Action<T>): Promise<T> {
     let action: Action<T> = otherAction;
 
